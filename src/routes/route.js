6b5c535d2e43a0a978ef08ab80dae22cf7259a61
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-07-18 13:33:40 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,4 +8,12 @@ router.post('/url/shorten', shortnerUrl)
 
 router.get('/:urlCode', getUrl)
 
+
+router.all("/**", function (req, res) {   
+    res.status(404).send({
+        status: false,
+        msg: "The api you request is not available"
+    })
+});
+
 module.exports = router;
