4de2560623b3ffdb5b325797c3dda0de04d7f7f2
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-07-18 13:26:53 +0530

    pushed by Rajat

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,47 +1,70 @@
 const urlModel = require('../models/urlModel');
- const shortId = require('shortid');
- const validUrl = require('validator');
+const shortId = require('shortid');
+const validUrl = require('validator');
+const shortid = require('shortid');
+const { trusted } = require('mongoose');
 
- function isValid(value) {
+function isValid(value) {
     if (typeof value !== 'string' || value.trim().length == 0) return true
     if (value == undefined || value == null) return true
     return false
 }
 
- exports.shortnerUrl = async (req,res) => {
-    try{
-           let data = req.body;
-           //valid data
-           if(Object.keys(data).length == 0) {
-                     return res.status(400).send({status: false , message: "Invalid Url please provide valid details"})
-           }
-        
-           //valid url data
-           if(isValid(data.longUrl)){
-            return res.status(400).send({status: false , message: "Please provide long URL"})
-           }
+exports.shortnerUrl = async(req, res) => {
+    try {
+        let data = req.body;
+        //valid data
+        if (Object.keys(data).length == 0) {
+            return res.status(400).send({ status: false, message: "Invalid Url please provide valid details" })
+        }
 
-           //checking for valid url
-           if(!validUrl.isURL(data.longUrl)){
-            return res.status(400).send({status: false , message: "Please provide valid URL"})
-           }
-           // generating URL code
-           let urlCode = shortId.generate().toLowerCase();
+        //valid url data
+        if (isValid(data.longUrl)) {
+            return res.status(400).send({ status: false, message: "Please provide long URL" })
+        }
 
-           //creating short URL
+        //checking for valid url
+        if (!validUrl.isURL(data.longUrl)) {
+            return res.status(400).send({ status: false, message: "Please provide valid URL" })
+        }
+        // generating URL code
+        let urlCode = shortId.generate().toLowerCase();
 
-           const shortUrl = `http://localhost:3000/${urlCode}`;
+        //creating short URL
 
-           data.urlCode = urlCode;
-           data.shortUrl = shortUrl;
+        const shortUrl = `http://localhost:3000/${urlCode}`;
 
-           //creating document or short url
-           await urlModel.create(data);
-           let responseData = await urlModel.findOne({urlCode : urlCode}).select({_id:0 , __v:0 , createdAt: 0 , updatedAt: 0 })
+        data.urlCode = urlCode;
+        data.shortUrl = shortUrl;
 
-          return res.status(201).send({status: true, message: "URL created succesfully" , data:responseData})
+        //creating document or short url
+        await urlModel.create(data);
+        let responseData = await urlModel.findOne({ urlCode: urlCode }).select({ _id: 0, __v: 0, createdAt: 0, updatedAt: 0 })
 
-    }catch(error){
-           return res.status(500).send({status: false, message: error.message})
+        return res.status(201).send({ status: true, message: "URL created succesfully", data: responseData })
+
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
     }
- }
+}
+
+exports.getUrl = async(req, res) => {
+    try {
+        const urlCode = req.params.urlCode
+
+        if (!shortid.isValid(urlCode)) {
+            return res.status(400).send({ status: false, message: "URL Code is not valid." })
+        }
+
+        const checkUrl = await urlModel.findOne({ urlCode: urlCode })
+
+        if (!checkUrl) {
+            return res.status(404).send({ status: false, message: "URL not found." })
+        }
+
+        return res.status(302).redirect(checkUrl.longUrl)
+
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+}
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const {shortnerUrl} = require('../controller/urlController');
+const { shortnerUrl, getUrl } = require('../controller/urlController');
 
-router.post('/url/shorten',shortnerUrl)
+router.post('/url/shorten', shortnerUrl)
+
+router.get('/:urlCode', getUrl)
 
 module.exports = router;
